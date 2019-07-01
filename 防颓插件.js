868ece95f02ffb1c3895a9418f16d5bbcdd24d24
KOG <42380934+TheKOG@users.noreply.github.com>
2019-07-01 21:24:34 +0800

    Create 防颓插件.js

---
+++ 防颓插件.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         防颓插件
+// @namespace    https://www.cnblogs.com/nlKOG
+// @version      0.1
+// @description  克制一时的颓废欲望
+// @author       The_KOG
+// @match        https://www.bilibili.com/*
+// @match        https://*.zhihu.com/*
+// @match        https://user.qzone.qq.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+	if(prompt('颓尼玛呢',"")!="dqyakioi"){
+        window.location.href='http://www.lydsy.com/';
+    }else{
+        alert("Yes he does!");
+    }
+})();
